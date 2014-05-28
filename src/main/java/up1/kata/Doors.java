d4fcc8cd192a4e46f4858fb7a04276ef1ed1c056
Somkiat <somkiat.p@gmail.com>
2014-05-28 10:47:25 +0700

    Refactor to extract constant to variable

--- src/main/java/up1/kata/Doors.java
+++ src/main/java/up1/kata/Doors.java
@@ -3,15 +3,17 @@ package up1.kata;
 public class Doors {
 
 	private int round;
-	
+	private final int MAX_DOOR = 100;
+
 	public Doors(int round) {
 		this.round = round;
 	}
 
 	public boolean[] getDoors() {
-		boolean[] doors = new boolean[100];
-		if(round ==0) return new boolean[100];
-		for(int i=0;i<100; i++) {
+		boolean[] doors = new boolean[MAX_DOOR];
+		if (round == 0)
+			return new boolean[MAX_DOOR];
+		for (int i = 0; i < MAX_DOOR; i++) {
 			doors[i] = true;
 		}
 		return doors;
