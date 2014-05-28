5323cd491c66130d99acc8e1dae0d46c374db493
Somkiat <somkiat.p@gmail.com>
2014-05-28 10:46:18 +0700

    Resolve second test case

--- src/main/java/up1/kata/Doors.java
+++ src/main/java/up1/kata/Doors.java
@@ -2,11 +2,19 @@ package up1.kata;
 
 public class Doors {
 
+	private int round;
+	
 	public Doors(int round) {
+		this.round = round;
 	}
 
 	public boolean[] getDoors() {
-		return new boolean[100];
+		boolean[] doors = new boolean[100];
+		if(round ==0) return new boolean[100];
+		for(int i=0;i<100; i++) {
+			doors[i] = true;
+		}
+		return doors;
 	}
 
 }
